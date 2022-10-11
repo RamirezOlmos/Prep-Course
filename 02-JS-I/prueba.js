13a2e71dffa100a29b071b0bd770516ae68ea8d9
RamirezOlmos <nyy_715074@yahoo.com>
2022-10-10 20:02:49 -0500

    changes in homework 02-JS-I

---
+++ 02-JS-I/prueba.js
@@ -0,0 +1,19 @@
+
+function esPositivo(numero) {
+  //La función va a recibir un entero. Devuelve como resultado una
+  //cadena de texto que indica si el número es positivo o negativo. 
+  //Si el número es positivo, devolver ---> "Es positivo"
+  //Si el número es negativo, devolver ---> "Es negativo"
+  //Si el número es 0, devuelve false
+    if(numero > 0)
+        return "Es positivo";
+    else if(numero < 0)
+        return "Es negativo";
+    else
+        return false;
+}
+
+console.log(esPositivo(5));
+console.log(esPositivo(-5));
+console.log(esPositivo(0));
+
