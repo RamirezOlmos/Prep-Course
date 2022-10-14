a075965b6eb907134809cbd069e743e00abab459
RamirezOlmos <nyy_715074@yahoo.com>
2022-10-14 12:56:36 -0500

    changes in homework 06-JS-V

--- 02-JS-I/prueba.js
+++ 02-JS-I/prueba.js
@@ -1,31 +1,55 @@
 
-function agregarMetodoCalculoDescuento (producto) {
-  // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
-  // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]")
-  // y "porcentajeDeDescuento" para obtener el descuento
-  // El método resta el descuento del precio y devuelve el precio con descuento
-  // Devuelve el objeto "producto" al final de la función
-  // Ejemplo:
-  // producto.precio -> 20
-  // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
-  // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
-  // Tu código:
-    producto["calcularPrecioDescuento"] = function() {
-        var precio_descuento = 0;
-        precio_descuento = this.precio - (this.precio * this.porcentajeDeDescuento);
-        return precio_descuento;
+  class Persona {
+    constructor(nombre, apellido, edad, domicilio) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.domicilio = domicilio;
     }
-    return producto;
+
+    detalle() {
+        var detail = {
+            nombre: this.nombre,
+            apellido: this.apellido,
+            edad: this.edad,
+            domicilio: this.domicilio,
+        }
+        return detail;
+    }
+
 }
 
+function crearInstanciaPersona(nombre, apellido, edad, dir) {
+  //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona
+  //(creado en el ejercicio anterior)
+  //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
+  //Devolver la nueva persona creada
+    const new_person = new Persona(nombre, apellido, edad, dir);
+
+    return new_person;
+}
+
+function agregarMetodo(){
+  //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la
+  //persona y devuelve: 
+  //Ej: "Juan, 22 años"
+    Persona.prototype.datos = function(){
+        return this.nombre + ", " + this.edad + " años";
+    }
+
+}
+
+const carlito = crearInstanciaPersona("Carlos", "Ramirez", 32, "Sutano 9");
 let arr = [1, 120, 133, 3, 50, 60];
 const car2 = { myCar: 'Saturn', precio: 25000, porcentajeDeDescuento: 0.10};
 const car3 = { myCar: 'Saturn', likes: 5};
 const car = { myCar: 'Saturn', posts: [car2, car3]};
 let arr1 = [car, car2];
+agregarMetodo();
 
-console.log(agregarMetodoCalculoDescuento(car2.calcularPrecioDescuento()));
-//console.log(actualizarPassword(car2, "1"));
+console.log(carlito.detalle());
+console.log(carlito.datos());
+//console.log(user.saludar());
 //console.log(mayorACien(arr1));
 //console.log(tieneTresDigitos(1));
 //console.log(multiplicarArgumentos(1, 2, 3));
